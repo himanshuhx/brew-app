4d59a37b32b55d9ee9b1b99c2619a5d11e9d94bd
Himanshu Suman <himanshuhx1221@gmail.com>
2023-11-07 19:22:42 +0530

    book module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
